e1f05a7dbca06203624a661dc5a2f180b2226ba9
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-12 23:48:04 +0100

    Setup of level

---
+++ BLACKJACKZACKGAME/Assets/BlackJackShuffle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackJackShuffle : MonoBehaviour
+{
+
+    public static string[] cardsuits = new string[] { "Spades" , "Hearts", "Diamonds", "Clubs"};
+    public static string[] cardvalues = new string[] {"2", "3", "4", "5", "6" ,"7", "8", "9", "10", "11"};
+
+    public List<string> carddeck;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
