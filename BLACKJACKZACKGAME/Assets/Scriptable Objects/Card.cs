46bf64e4050e03a0785ba67a0002b1fb29038a3b
Patr30t_Mark_Shope <160268@virtualwindows.co.za>
2021-03-12 23:06:19 +0100

    setup of card asssets

---
+++ BLACKJACKZACKGAME/Assets/Scriptable Objects/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Card : ScriptableObject 
+{
+
+    public new string name;
+    public string cardType;
+
+    public Sprite artwork;
+
+    public int  cardValue;
+    // Start is called before the first frame update
+   
+}
